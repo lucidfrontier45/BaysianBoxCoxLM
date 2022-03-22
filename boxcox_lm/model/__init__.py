59a1e264ccf955694cf0611668b9a0318efdbfd3
Du Shiqiao <lucidfrontier.45@gmail.com>
2022-03-22 09:28:35 +0900

    add cmd options

--- boxcox_lm/model/__init__.py
+++ boxcox_lm/model/__init__.py
@@ -5,7 +5,7 @@ from .cmdstanpy import CmdStanPyModel
 from .numpyro import NumPyroModel
 
 
-class BackendType(enum.Enum):
+class BackendType(str, enum.Enum):
     NumPyro = "numpyro"
     CmdStanPy = "cmdstanpy"
 
@@ -16,9 +16,9 @@ _backends = {BackendType.NumPyro: NumPyroModel, BackendType.CmdStanPy: CmdStanPy
 class BayesianBoxCoxLinearRegression(BayesianModel):
     def __init__(
         self,
-        w_s: float = 2,
-        sigma_beta: float = 2,
-        lambda_s: float = 1,
+        w_s: float = 2.0,
+        sigma_beta: float = 2.0,
+        lambda_s: float = 1.0,
         n_warmup: int = 500,
         n_samples: int = 1000,
         n_chains: int = 4,
--- main.py
+++ main.py
@@ -1,35 +1,55 @@
+from typing import Optional
+
 import numpy as np
 import numpyro
+import typer
 
 from boxcox_lm import BackendType, BayesianBoxCoxLinearRegression
 
-numpyro.set_host_device_count(4)
-
-seed = 0
-np.random.seed(seed=seed)
-N = 20
-D = 2
-
-w = np.array([5.0, -1.2])
-X = np.random.randn(N, D - 1) * 0.1
-X = np.column_stack([np.ones(N), X])
-z = np.dot(X, w)
-y = np.array([np.random.poisson(zz) for zz in np.exp(z)])
-
-params = {
-    "w_s": 2,
-    "sigma_beta": 2,
-    "lambda_s": 1,
-    "n_warmup": 500,
-    "n_samples": 1000,
-    "n_chains": 4,
-}
+app = typer.Typer(name="baysian_boxcox_regression", add_completion=False)
 
 
-model = BayesianBoxCoxLinearRegression(backend=BackendType.CmdStanPy, **params)
-model.fit(X, y)
-model.summary()
-posterior_y = model.predict(X).mean(0)
+@app.command()
+def main(
+    n_data: int = 50,
+    w_s: float = 2.0,
+    sigma_beta: float = 2.0,
+    lambda_s: float = 1.0,
+    n_warmup: int = 500,
+    n_samples: int = 1000,
+    n_chains: int = 4,
+    seed: Optional[int] = None,
+    backend: BackendType = typer.Option(BackendType.NumPyro, case_sensitive=False),
+):
 
-for (yy, pyy) in zip(y, posterior_y):
-    print(yy, pyy)
+    numpyro.set_host_device_count(4)
+
+    seed = 0
+    np.random.seed(seed=seed)
+    N = n_data
+    D = 2
+
+    w = np.array([5.0, -1.2])
+    X = np.random.randn(N, D - 1) * 0.1
+    X = np.column_stack([np.ones(N), X])
+    z = np.dot(X, w)
+    y = np.array([np.random.poisson(zz) for zz in np.exp(z)])
+
+    model = BayesianBoxCoxLinearRegression(
+        w_s=w_s,
+        sigma_beta=sigma_beta,
+        lambda_s=lambda_s,
+        n_warmup=n_warmup,
+        n_samples=n_samples,
+        n_chains=n_chains,
+        backend=backend,
+    )
+    model.fit(X, y)
+    model.summary()
+    posterior_y = model.predict(X).mean(0)
+
+    for (yy, pyy) in zip(y, posterior_y):
+        print(yy, pyy)
+
+
+app()
